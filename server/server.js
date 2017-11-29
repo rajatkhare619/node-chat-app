16a2a553784ae2a3e8d15258d7fce84d8886d434
Rajat Khare <rajatkhare619@gmail.com>
2017-11-29 19:55:41 +0530

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
 console.log("new user connected");
+
+socket.emit('newMessage', {
+    from: "admin",
+    text: "welcome rajat",
+    createdAt: new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage', {
+    from: "admin",
+    text: "rajat joined",
+    createdAt: new Date().getTime()
+});
+
     socket.on('createMessage', (newMessage) => {
         console.log("newMessage event", newMessage);
         io.emit('newMessage', {
@@ -21,6 +34,11 @@ console.log("new user connected");
             text: newMessage.text,
             createdAt: new Date().getTime()
         });
+        /*socket.broadcast.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });*/
     });
 
     socket.on('disconnect', () => {
